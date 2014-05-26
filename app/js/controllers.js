eab1d45b866429a85858dbebd5a36e265baa14b9
gtrivedi <trivedigaurav@gmail.com>
2014-05-26 00:01:40 -0400

    Added tabbed view for docs and wordtrees

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,4 +8,16 @@ angular.module('myApp.controllers', [])
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
 
-  }]);
+  }])
+  .controller('TabsDemoCtrl', ['$scope', function($scope) {
+      $scope.tabs = [
+        { title:'Dynamic Title 1', content:'Dynamic content 1' },
+        { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+      ];
+
+      $scope.alertMe = function() {
+        setTimeout(function() {
+          alert('You\'ve selected the alert tab!');
+        });
+      };
+   }]);
