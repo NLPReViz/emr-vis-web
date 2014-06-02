17eb99573c5184dc0ace298e78b2aba7cffb9270
gtrivedi <trivedigaurav@gmail.com>
2014-06-02 13:33:12 -0400

    Fix tabbing

--- app/js/directives.js
+++ app/js/directives.js
@@ -4,27 +4,27 @@
 
 
 angular.module('myApp.directives', [])
-  .directive('horizontalSplitter', ['$document', function($document) {
+.directive('horizontalSplitter', ['$document', function($document) {
     return function(scope, element, attr) {
       var startY = 0, y = 0;
 
       if(!angular.isFunction($(element).offset)) {
         throw new Error('Need jquery!');
-      }
+    }
 
-      var topMin = 0.1*$(element).prev().height();
-      var bottomMin = 0.1*$(element).next().height();
+    var topMin = 0.1*$(element).prev().height();
+    var bottomMin = 0.1*$(element).next().height();
 
-      element.on('mousedown', function(event) {
+    element.on('mousedown', function(event) {
         // Prevent default dragging of selected content
         event.preventDefault();
         startY = event.pageY;
 
         $document.on('mousemove', mousemove);
         $document.on('mouseup', mouseup);
-      });
+    });
 
-      function mousemove(event) {
+    function mousemove(event) {
         y = event.pageY - startY;
         startY = event.pageY;
 
@@ -39,33 +39,32 @@ angular.module('myApp.directives', [])
             $(element).next().height(bottomHeight);
             $("#sidebar-top").trigger('heightChange'); 
         }
-      }
-
-      function mouseup() {
-        $document.off('mousemove', mousemove);
-        $document.off('mouseup', mouseup);
-      }
-    };
-  }])
- 
-  .directive('highlightReport', ['$document', function($document) {
-
-    function link(scope, element, attr) {
-        console.log("I was called");
-        element.text(scope.reportText);
-        $(element).highlight(/ascending/gi, "highlight positive")
-                .highlight(/colonoscopy/gi, "highlight negative")
-                .highlight(/.*\:/gi, "dim")
-                .highlight(/S_O_H\s\SE_O_H/gi, "dim")
-                .highlight(/De-ID.*S_O_H/gi, "dim")
-                .highlight(/.*E_O_H/gi, "dim")
-                .highlight(/\[Report de-identified.*/gi, "dim")
-                .highlight(/\*\*.*/gi, "dim")
-                .highlight(/E_O_R/gi, "dim");
     }
 
-    return {
+    function mouseup() {
+        $document.off('mousemove', mousemove);
+        $document.off('mouseup', mouseup);
+    }
+};
+}])
+
+.directive('highlightReport', ['$document', function($document) {
+    function link(scope, element, attr) {
+        element.text(scope.reportText);
+      
+        $(element).highlight(/ascending/gi, "highlight positive")
+            .highlight(/colonoscopy/gi, "highlight negative")
+            .highlight(/.*\:/gi, "dim")
+            .highlight(/S_O_H\s\SE_O_H/gi, "dim")
+            .highlight(/De-ID.*S_O_H/gi, "dim")
+            .highlight(/.*E_O_H/gi, "dim")
+            .highlight(/\[Report de-identified.*/gi, "dim")
+            .highlight(/\*\*.*/gi, "dim")
+            .highlight(/E_O_R/gi, "dim");
+  }
+
+  return {
       link: link
-    };
-  }]);
+  };
+}]);
 
