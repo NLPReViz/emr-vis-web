50f55eee08220a4b7105feb1da393f5291e13566
gtrivedi <trivedigaurav@gmail.com>
2014-06-05 17:28:25 -0400

    UNSAFE: Don't bind to localhost; exposes server on LAN

--- random-grid.py
+++ random-grid.py
@@ -66,5 +66,5 @@ for doc in docIds:
     # data.append(document)
     data[doc] = document
 
-print json.dumps(data)
-# print json.dumps(data, indent=4, sort_keys=True)
+# print json.dumps(data)
+print json.dumps(data, indent=4, sort_keys=True)
