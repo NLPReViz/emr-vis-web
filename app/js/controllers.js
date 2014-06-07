8ab09bf7c7fad433e644adbe2aad8e629085a1ca
gtrivedi <trivedigaurav@gmail.com>
2014-06-06 20:54:25 -0400

    Using ng-class instead of jquery

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -63,27 +63,13 @@ angular.module('myApp.controllers', [])
     $scope.updateGrid = function(variable, activeDocIndex) {
         // console.log(variable, activeDoc);
         if(variable != $scope.variable) {
-          $("#cell-"+$scope.activeVariable+"-"+$scope.activeDocIndex)
-                        .removeClass("selected");
-
           $scope.activeVariable = variable;
           $scope.loadDistribution(variable);
-
-          $("#cell-"+variable+"-"+$scope.activeDocIndex)
-                .addClass("selected")
         }
 
         if(activeDocIndex != $scope.activeDocIndex) {
-
-          $("#cell-"+$scope.activeVariable+"-"+$scope.activeDocIndex)
-                        .removeClass("selected");
-          
-          // $scope.activeDoc = $scope.gridData[activeDocIndex].id
           $scope.activeDocIndex = activeDocIndex;
           $scope.loadReport(activeDocIndex);
-
-          $("#cell-"+variable+"-"+$scope.activeDocIndex)
-                .addClass("selected")
         }
     }
 
