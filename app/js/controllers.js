4d67f364b408cb96e4e4fec92ae2f7a2e72950cf
gtrivedi <trivedigaurav@gmail.com>
2014-06-09 13:01:28 -0400

    Fix tabbing

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,202 +3,202 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MainCtrl', ['$scope', '$http', '$window', '$document', function($scope, $http, $window, $document) {
-    /*
-     * App config
-     */
+  .controller('MainCtrl', ['$scope', '$http', '$window', '$document', 
+    function($scope, $http, $window, $document) {
+        /*
+         * App config
+         */
 
-    //TODO: Move them to configs - affects perf
-    $scope.classificationName = {"positive": "True", "negative": "False"};
+        //TODO: Move them to configs - affects perf
+        $scope.classificationName = {"positive": "True", "negative": "False"};
 
-    $scope.variables = ["any-adenoma", "appendiceal-orifice", "asa", "biopsy", "cecum",
-              "ileo-cecal-valve", "indication-type", "infomed-consent", 
-              "nursing-report", "no-prep-adequate", "not-prep-adequate",
-              "yes-prep-adequate", "proc-aborted", "widthdraw-time"]
+        $scope.variables = ["any-adenoma", "appendiceal-orifice", "asa", "biopsy", "cecum",
+                  "ileo-cecal-valve", "indication-type", "infomed-consent", 
+                  "nursing-report", "no-prep-adequate", "not-prep-adequate",
+                  "yes-prep-adequate", "proc-aborted", "widthdraw-time"]
 
-    /*
-     * Main grid
-     */
+        /*
+         * Main grid
+         */
 
-    $scope.activeDocIndex = null;
-    $scope.showGrid = true;
-
-    $scope.appLoading = true;
-    $scope.appDisabled = false;
-
-    $http.get("dummy-grid.json")
-        .success(function(data, status) {
-            $scope.gridData = data;
-            
-            //Show first report in the set
-            $scope.activeDocIndex = 0;
-            $scope.loadReport(0);
-            
-        })
-        .error(function() { alert("Could not load grid data!"); });
-
-    $http.get("dummy-variable.json")
-        .success(function(data, status) {
-            $scope.variableData = data;
-
-            $scope.variables.forEach(function(variable) {
-              // console.log(data[variable]["numPositive"]);
-              $scope.variableData[variable]["percPositive"] = 
-                                      Math.round(100.0 * data[variable]["numPositive"] / 
-                                      (data[variable]["numPositive"] + data[variable]["numNegative"]));
-              $scope.variableData[variable]['percNegative'] = 100.0 - $scope.variableData[variable]['percPositive'];
-
-              // console.log($scope.variableData[variable]["percPositive"] + " - " + $scope.variableData[variable]["percNegative"]);
-                                      
-            });
-
-            $scope.activeVariable = "asa";
-            $scope.loadDistribution("asa");
-        })
-        .error(function() { alert("Could not load variable data!"); });
-
-    $scope.styleGridCell = function(classification, confidence) {
-        if (classification == "positive") {
-            if (confidence > 0.5) 
-                return "cert1-pos";
-            else
-                return "cert0-pos";
-        }
-        else {
-            if (confidence > 0.5) 
-                return "cert1-neg";
-            else
-                return "cert0-neg";
-        }
-    };
-
-    $scope.updateGrid = function(variable, activeDocIndex) {
-        // console.log(variable, activeDoc);
-        if(variable != $scope.variable) {
-          $scope.activeVariable = variable;
-          $scope.loadDistribution(variable);
-        }
-
-        if(activeDocIndex != $scope.activeDocIndex) {
-          $scope.activeDocIndex = activeDocIndex;
-          $scope.loadReport(activeDocIndex);
-        }
-    };
-
-    /*
-     * Load reports
-     */
-
-    //TODO: Load reports not as variables but as docs
-    $scope.loadReport = function(activeDocIndex) {
-
-        var activeDoc = $scope.gridData[activeDocIndex].id;
-
-        // $scope.reportPath = "docs/"+ activeDoc +"/report.txt";
-        // $scope.pathologyPath = "docs/"+ activeDoc +"/pathology.txt";
-
-        $scope.reportExists = false;
-        $scope.pathologyExists = false;
+        $scope.activeDocIndex = null;
+        $scope.showGrid = true;
 
         $scope.appLoading = true;
+        $scope.appDisabled = false;
 
-        //report
-        $http.get("docs/"+activeDoc+"/report.txt")
+        $http.get("dummy-grid.json")
             .success(function(data, status) {
-                $scope.reportText = data;
-                $scope.reportExists = true;
-
-                $scope.appLoading = false;
-                $scope.feedbackText = null;
+                $scope.gridData = data;
+                
+                //Show first report in the set
+                $scope.activeDocIndex = 0;
+                $scope.loadReport(0);
+                
             })
-            .error(function(data, status, headers, config) {
-                $scope.reportText = "Status " + status
-                alert($scope.reportPath + " is not accessible. Make sure you have the docs/ folder in the app/ directory.");
+            .error(function() { alert("Could not load grid data!"); });
 
-                $scope.appLoading = false;
-            });
-
-        // pathology
-        $http.get("docs/"+activeDoc+"/pathology.txt")
+        $http.get("dummy-variable.json")
             .success(function(data, status) {
-                $scope.pathologyText = data;
-                $scope.pathologyExists = true;
+                $scope.variableData = data;
+
+                $scope.variables.forEach(function(variable) {
+                  // console.log(data[variable]["numPositive"]);
+                  $scope.variableData[variable]["percPositive"] = 
+                                          Math.round(100.0 * data[variable]["numPositive"] / 
+                                          (data[variable]["numPositive"] + data[variable]["numNegative"]));
+                  $scope.variableData[variable]['percNegative'] = 100.0 - $scope.variableData[variable]['percPositive'];
+
+                  // console.log($scope.variableData[variable]["percPositive"] + " - " + $scope.variableData[variable]["percNegative"]);
+                                          
+                });
+
+                $scope.activeVariable = "asa";
+                $scope.loadDistribution("asa");
             })
-    };
+            .error(function() { alert("Could not load variable data!"); });
 
-    /*
-     * Pie chart
-     */
+        $scope.styleGridCell = function(classification, confidence) {
+            if (classification == "positive") {
+                if (confidence > 0.5) 
+                    return "cert1-pos";
+                else
+                    return "cert0-pos";
+            }
+            else {
+                if (confidence > 0.5) 
+                    return "cert1-neg";
+                else
+                    return "cert0-neg";
+            }
+        };
 
-    $scope.pieData = [
-        {name: "!#def", count: 1, classification: "positive"},
-     ]; 
+        $scope.updateGrid = function(variable, activeDocIndex) {
+            // console.log(variable, activeDoc);
+            if(variable != $scope.variable) {
+              $scope.activeVariable = variable;
+              $scope.loadDistribution(variable);
+            }
 
-    $scope.loadDistribution = function(variable) {
+            if(activeDocIndex != $scope.activeDocIndex) {
+              $scope.activeDocIndex = activeDocIndex;
+              $scope.loadReport(activeDocIndex);
+            }
+        };
+
+        /*
+         * Load reports
+         */
+
+        //TODO: Load reports not as variables but as docs
+        $scope.loadReport = function(activeDocIndex) {
+
+            var activeDoc = $scope.gridData[activeDocIndex].id;
+
+            // $scope.reportPath = "docs/"+ activeDoc +"/report.txt";
+            // $scope.pathologyPath = "docs/"+ activeDoc +"/pathology.txt";
+
+            $scope.reportExists = false;
+            $scope.pathologyExists = false;
+
+            $scope.appLoading = true;
+
+            //report
+            $http.get("docs/"+activeDoc+"/report.txt")
+                .success(function(data, status) {
+                    $scope.reportText = data;
+                    $scope.reportExists = true;
+
+                    $scope.appLoading = false;
+                    $scope.feedbackText = null;
+                })
+                .error(function(data, status, headers, config) {
+                    $scope.reportText = "Status " + status
+                    alert($scope.reportPath + " is not accessible. Make sure you have the docs/ folder in the app/ directory.");
+
+                    $scope.appLoading = false;
+                });
+
+            // pathology
+            $http.get("docs/"+activeDoc+"/pathology.txt")
+                .success(function(data, status) {
+                    $scope.pathologyText = data;
+                    $scope.pathologyExists = true;
+                })
+        };
+
+        /*
+         * Pie chart
+         */
+
         $scope.pieData = [
-          {name: $scope.classificationName["positive"], count: $scope.variableData[variable]["numPositive"], classification: "positive"},
-          {name: $scope.classificationName["negative"], count: $scope.variableData[variable]["numNegative"], classification: "negative"},
-        ];
+            {name: "!#def", count: 1, classification: "positive"},
+         ]; 
 
-        $scope.pieData.sort(function(first, second) {
-          return second.count - first.count;
-        });
-    };
+        $scope.loadDistribution = function(variable) {
+            $scope.pieData = [
+              {name: $scope.classificationName["positive"], count: $scope.variableData[variable]["numPositive"], classification: "positive"},
+              {name: $scope.classificationName["negative"], count: $scope.variableData[variable]["numNegative"], classification: "negative"},
+            ];
 
-    /*
-     * Feedback
-     */
+            $scope.pieData.sort(function(first, second) {
+              return second.count - first.count;
+            });
+        };
 
-    $scope.numFeedback = 42;
-    $scope.feedbackText = false;
+        /*
+         * Feedback
+         */
 
-    $scope.setFeedbackText = function(){
-        var text = '';
+        $scope.numFeedback = 42;
+        $scope.feedbackText = false;
 
-        // Adapted from http://stackoverflow.com/questions/4652734/return-html-from-a-user-selected-text/4652824#4652824
-        if (typeof $window.getSelection != "undefined") {
-            var sel = $window.getSelection();
-            if (sel.rangeCount) {
-                for (var i = 0, len = sel.rangeCount; i < len; ++i) {
-                    text = " " + sel.getRangeAt(i).toString();
+        $scope.setFeedbackText = function(){
+            var text = '';
+
+            // Adapted from http://stackoverflow.com/questions/4652734/return-html-from-a-user-selected-text/4652824#4652824
+            if (typeof $window.getSelection != "undefined") {
+                var sel = $window.getSelection();
+                if (sel.rangeCount) {
+                    for (var i = 0, len = sel.rangeCount; i < len; ++i) {
+                        text = " " + sel.getRangeAt(i).toString();
+                    }
+                }
+            } else if (typeof $document.selection != "undefined") {
+                if ($document.selection.type == "Text") {
+                    text = $document.selection.createRange().text;
                 }
             }
-        } else if (typeof $document.selection != "undefined") {
-            if ($document.selection.type == "Text") {
-                text = $document.selection.createRange().text;
+
+            text = text.trim();
+            // console.log("setFeedbackText: " + text);
+
+            if (text) {
+                $scope.feedbackText = text;
             }
-        }
-
-        text = text.trim();
-        // console.log("setFeedbackText: " + text);
-
-        if (text) {
-            $scope.feedbackText = text;
-        }
-        else{
-            $scope.feedbackText = false;
-        }
-    };
-
-  }])
+            else{
+                $scope.feedbackText = false;
+            }
+        };
+    }])
 
   // }])
-  .controller('TabsDemoCtrl', ['$scope', function($scope) {
-      // $scope.tabs = [
-      //   { title:'WordTree View', content:'Dynamic content 1' },
-      //   { title:'Review Feedback <span class="badge pull-right">42</span>', content:'Dynamic content 2', disabled: true }
-      // ];
+    .controller('TabsDemoCtrl', ['$scope', function($scope) {
+        // $scope.tabs = [
+        //   { title:'WordTree View', content:'Dynamic content 1' },
+        //   { title:'Review Feedback <span class="badge pull-right">42</span>', content:'Dynamic content 2', disabled: true }
+        // ];
 
-      $scope.bDocView = true;
+        $scope.bDocView = true;
 
-      $scope.alertMe = function() {
+        $scope.alertMe = function() {
         $scope.numFeedback = 0;
         setTimeout(function() {
           alert('Re-training!');
         });
-      };
+        };
 
-      $scope.toggleDocView = function(state){
+        $scope.toggleDocView = function(state){
         $scope.bDocView = state;
-      }
-   }]);
+        }
+    }]);
