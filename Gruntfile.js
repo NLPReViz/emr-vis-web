8c3f4c401d53ae7b711193a45868a12f13771d04
gtrivedi <trivedigaurav@gmail.com>
2014-06-13 11:52:28 -0400

    Added grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function (grunt) {
+  var backend = process.env.npm_package_config_backend;
+  var appFolder = process.env.npm_package_config_appFolder;
+
+  grunt.loadNpmTasks('grunt-replace');
+  grunt.initConfig({
+    replace: {
+      dist: {
+        options: {
+          patterns: [
+            {
+              match: 'backEndApp',
+              replacement: backend
+            }
+          ]
+        },
+        files: [
+          {
+            expand: true, 
+            flatten: true, 
+            src: ['./config/services.js'], 
+            dest: appFolder + '/js/'}
+        ]
+      }
+    }
+  });
+  grunt.registerTask('default', 'replace');
+};
+
