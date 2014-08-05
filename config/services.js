b71e761d5ce38954ee90c1b2cb0a58a1b1e287d9
gtrivedi <trivedigaurav@gmail.com>
2014-08-05 13:07:36 -0400

    Handle trigger happy users

--- config/services.js
+++ config/services.js
@@ -3,7 +3,7 @@
 /* Services */
 
 angular.module('myApp.services', [])
-    .factory('backend', function($http) {
+    .factory('backend', function($http, $q) {
     return {
         getVarDatasetList: function() {
             return $http.get('@@backEndApp/getVarDatasetList/')
@@ -12,27 +12,41 @@ angular.module('myApp.services', [])
                         });
         },
         getGridData: function(model, dataset) {
-            return $http.get("@@backEndApp/getVarGridObj/" + model + ".xml/" + dataset + ".xml")
-                       .then(function(result) {
+            if(typeof this.gridDataCanceler != 'undefined')
+                this.gridDataCanceler.resolve();
+
+            this.gridDataCanceler = $q.defer();
+            return $http.get("@@backEndApp/getVarGridObj/" + model + ".xml/" + dataset + ".xml",
+                            {timeout: this.gridDataCanceler.promise})
+                        .then(function(result) {
                             return result.data;
                         });
         },
         getReport: function(report) {
-            return $http.get("@@backEndApp/getReport/" + report)
-                       .then(function(result) {
+            if(typeof this.reportCanceler != 'undefined')
+                this.reportCanceler.resolve();
+
+            this.reportCanceler = $q.defer();
+            return $http.get("@@backEndApp/getReport/" + report, 
+                            {timeout: this.reportCanceler.promise})
+                        .then(function(result) {
                             return result.data;
                         });
         },
         getWordTree: function(dataset, query) {
-            return $http.get('@@backEndApp/getWordTree/' + dataset + ".xml/" + query)
-                       .then(function(result) {
+            if(typeof this.wordTreeCanceler != 'undefined')
+                this.wordTreeCanceler.resolve();
+
+            this.wordTreeCanceler = $q.defer();
+            return $http.get('@@backEndApp/getWordTree/' + dataset + ".xml/" + query, 
+                            {timeout: this.wordTreeCanceler.promise})
+                        .then(function(result) {
                             return result.data;
                         });
         },
         putFeedback: function(feedbackList, model, dataset) {
-            //ignore dataset for now
             return $http.put("@@backEndApp/putFeedback/" + model + ".xml/" + dataset + ".xml", 
-                        angular.toJson(feedbackList))
+                            angular.toJson(feedbackList))
                         .then(function(result) {
                             return result.data;
                         });
